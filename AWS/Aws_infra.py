4d0c76978ddd14e7e3ac7e32c101b3b88a35adb9
limitededition43 <limitededition43@icloud.com>
2021-07-02 05:32:33 +0530

    Minor update

--- AWS/Aws_infra.py
+++ AWS/Aws_infra.py
@@ -2,41 +2,72 @@
 
 import boto3
 import json
-
 from boto.utils import get_instance_metadata
-id=""
-key=""
 
-details = ["VpcId","CidrBlock","State","IsDefault","Tags"]
-client = boto3.client('ec2',aws_access_key_id=id,aws_secret_access_key=key,region_name='us-east-1')
-ec2=  boto3.resource('ec2',aws_access_key_id=id,aws_secret_access_key=key,region_name='us-east-1')
+id = ''
+key = ''
+region = ''
+
+SEP = '--------------------------------------------------------------------------------------------------------------------------------------'
+VPCdetails = ["VpcId","CidrBlock","State","IsDefault","Tags"]
+RTdetails = ["RouteTableId","VpcId","Associations","Tags","Routes"]
+client = boto3.client('ec2',aws_access_key_id=id,aws_secret_access_key=key,region_name=region)
+ec2 =  boto3.resource('ec2',aws_access_key_id=id,aws_secret_access_key=key,region_name=region)
+
+
+class bcolors:
+    H = '\033[95m'
+    OB = '\033[94m'
+    OC = '\033[96m'
+    OG = '\033[92m'
+    W = '\033[93m'
+    F = '\033[91m'
+    E = '\033[0m'
+    B = '\033[1m'
+    U = '\033[4m'
 
 def getVPC():
 	response = client.describe_vpcs()
-	resp = response['Vpcs']
-	print("Found %s VPC" % len(resp))
-	print("Ignoring Default VPC")
-	for i in range(len(resp)):
+	Vpcs = response.get('Vpcs')
+	print(bcolors.U + SEP + bcolors.E)
+	print(bcolors.OG + "[+] Found %s VPC" % len(Vpcs) + bcolors.E)
+	print(bcolors.W + "[+] Ignoring Default VPC" + bcolors.E)
+	for i in range(0,len(Vpcs)):
 		VPC = dict()
-		if not resp[i]['IsDefault']:
-			for d in details:
-				if not (d == "Tags"):
-					VPC.update({d:resp[i][d]})
-				VPC.update({"Name":resp[i][d]})
-
-			print("\n\t[+] VPC: ",VPC)
-			print("\t\t[+] Routes:", getRoutes(VPC['VpcId']))
-			print("\t\t[+] Subnets:", getSubnet(VPC['VpcId']))
+		if not Vpcs[i]['IsDefault']:
+			for d in VPCdetails:
+				if (d == "Tags"):
+					VPC.update({"Name":Vpcs[i][d][0]['Value']})
+					continue
+				VPC.update({d:Vpcs[i][d]})
+			print(SEP)
+			print(bcolors.OC + "\n\t[+] VPC:\n " +bcolors.E,json.dumps(VPC,indent=4))
+			print(bcolors.OB + "\t\t[*] Routes:\n" +bcolors.E,json.dumps(getRoutes(VPC['VpcId']),indent=16))
+#			print("\t\t[+] Subnets:", getSubnet(VPC['VpcId']))
 
 
 def getRoutes(id):
 	vpc = ec2.Vpc('id')
 	route_tables = client.describe_route_tables()
-	routes = []
+
 	for r in range(len(route_tables)):
-		if route_tables['RouteTables'][r]['VpcId'] == id:
-			routes.append(route_tables['RouteTables'][r]['Routes'][1])
-	return routes
+		routes = dict()
+		for d in RTdetails:
+			if (d == 'Associations'):
+				subdetails = ['AssociationState','Main','SubnetId']
+				for i in range(0,len(subdetails)-1):
+					for s in subdetails:
+						if s not in route_tables['RouteTables'][r][d][i].keys():
+							continue
+						routes.update({d:route_tables['RouteTables'][r][d][i][s]}) 
+			elif (d == 'Routes'):
+				subdetails =['DestinationCidrBlock','GatewayId','State']
+				for i in range(0,len(subdetails)):
+#					for s in subdetails:
+					routes.update({d:route_tables['RouteTables'][r][d]})
+
+			routes.update({d:route_tables['RouteTables'][r][d]})
+		return routes
 
 
 def getSubnet(id):
